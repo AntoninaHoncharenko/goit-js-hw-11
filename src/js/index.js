334d7dec5f0b4c62556bced7b2037f737f37060f
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-17 18:39:34 +0300

    add libs

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 import '../css/styles.css';
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
