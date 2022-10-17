3c47ce6fedabca18e34a0bddd5aad1133b4a5280
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-17 21:47:41 +0300

    update

---
+++ src/js/APIservice.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export class ApiService {
+  constructor() {
+    this.searchQuery = '';
+  }
+  async fetchImages() {
+    //   axios.defaults.baseURL = 'https://pixabay.com/api/';
+    //   axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+    const inputValue = event.currentTarget.elements.searchQuery.value;
+    const BASE_URL = 'https://pixabay.com/api/';
+    const KEY = '30659644-d62c8c976bf0a1f367dc53c1a';
+    const url = `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+
+    const { data } = await axios.get(url);
+    console.log(data);
+    return data;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,48 @@
 import '../css/styles.css';
-import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createMarkup } from './createMarkup';
+import { ApiService } from './APIservice';
+
+const apiService = new ApiService();
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  loadmoreBtn: document.querySelector('.load-more'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+  apiService.query = event.currentTarget.elements.searchQuery.value.trim();
+
+  refs.form.reset();
+
+  try {
+    if (apiService.searchQuery === '') {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+        { clickToClose: true }
+      );
+      return;
+    }
+
+    const { hits, totalHits } = await apiService.fetchImages();
+    console.log(hits);
+    console.log(totalHits);
+
+    if (hits.length < 1) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+        { clickToClose: true }
+      );
+      return;
+    }
+
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+  } catch (error) {
+    console.log(error);
+  }
+}
